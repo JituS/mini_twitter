bab648e6d20a7922e8d8b7c399fb3970aa618982
= <=>
2016-03-01 19:34:17 +0530

    [jitendra] - Login page UI improved
    What changes have you made?
    
    Previously login page was not intractive as much,
    Now it looks little bit fine
    
     Changes to be committed:
    	modified:   lib/routes.js
    	modified:   npm-debug.log
    	modified:   public/css/index.css
    	modified:   public/css/main.css
    	modified:   public/css/style.css
    	new file:   public/index.html
    	modified:   public/javascript/index.js
    	modified:   public/javascript/main.js
    	new file:   public/main.html
    	new file:   public/saaa.gif
    
     Changes not staged for commit:
    	deleted:    public/html/index.html
    	deleted:    public/html/main.html
    	deleted:    public/html/saaa.gif

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,16 +2,17 @@ var loginReq = function(name, password){
 	if(name && password){
 		$.post('login', 'name='+name+'&password='+password, function(data, status){
 			console.log(data);
-			if(status == 'success' && data.link){
+			if(status == 'success' && data.error){
+				alert('enter correct username and password');
+			}else{
 				window.location.href = data.link;
 			}
 		});
-	}else
-		alert('enter correct username and password');
+	}
 };
 
-var signupReq = function(name, email, password){
-	$.post('signup', 'name='+name+'&email='+email+'&password='+password, function(data, status){
+var signupReq = function(name, email, password, dob){
+	$.post('signup', 'name='+name+'&email='+email+'&password='+password+'&dob='+dob, function(data, status){
 		if(status == 'success')
 			alert(data);
 	});
@@ -25,6 +26,7 @@ $(document).ready(function(){
 		var name = $('input[name=sname]').val();
 		var email = $('input[name=email]').val();
 		var password = $('input[name=spassword]').val();
-		signupReq(name, email, password);
+		var dob = $('input[name=sdob]').val();
+		signupReq(name, email, password, dob);
 	});		
 });	
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -5,40 +5,61 @@ var like = function(value){
 }
 
 var likersTable = function(data){
-	var template = '<table><tr><th>Name </th><th>Email </th></tr>_TR_</table>';
+	var template = '<a href="#" id="close">close</a><table><tr><th>Name </th><th>Email </th></tr>_TR_</table>';
 	var tableData = data.map(function(user){
 		return '<tr><td>'+user.username+'</td><td>'+user.email+'</td></tr>'
 	});
-	console.log(tableData)
 	return template.replace('_TR_', tableData.join(''));
 };
 
 var likers = function(value){
 	$.post('likers', 'postId='+value, function(data, status){
-			console.log(data)
 		if(status == 'success'){
-			$('#likers').html(likersTable(data));
+			$('#top').removeClass('hide');
+			$('.message').html(likersTable(data));
+			$('#close').on('click',close);
 		}
 	});
 };
 
+var renderDetails = function(data){
+	var template = '<a href="#" id="close">close</a><ul>_DATA_</ul>';
+	var list = "";
+	for(var i in data){
+		if(i == "dob") data[i] = data[i].split("T")[0];
+		list += "<li>" + i.toUpperCase() + " : " + data[i] + "</li><br>";
+	}
+	template = template.replace("_DATA_", list);
+	$('#top').removeClass('hide');
+	$('.message').html(template);
+	$('#close').on('click',close);
+
+}
+
+var getDetail = function(userid){
+	$.post('getDetail','id='+userid ,function(data, status){
+		if(status == 'success'){
+			renderDetails(data);
+		}
+	});
+}
+
 var renderAllMsg = function(data){
-	data = JSON.parse(data);
 	var a = data.map(function(each){
-		return '<div class="post"><div id="date">date: '+each.moment.split('T')[0]+'</div><div id="name">'+
-			each.username+'</div><div id="content"><p>'+each.content+'</p></div><div class="like"><img src="./saaa.gif"value='+
-			each.postid+' onclick=like(this.getAttribute("value"))></img><div id=likes value='+each.postid+
-			' onclick=likers(this.getAttribute("value"))>'+each.likes+'</div></div></div>';
+		return '<div class="post"><div id="date">date: '+each.moment.split('T')[0]+'</div><div class="content"><p><a href="#" id='+each.userid+' onclick=getDetail(this.getAttribute("id"))'+'>'+
+				each.username+'</a>@'+each.content+'</p></div><div class="like"><img src="./saaa.gif"value='+
+				each.postid+' onclick=like(this.getAttribute("value"))></img><div id=likes value='+each.postid+
+				' onclick=likers(this.getAttribute("value"))>&nbsp<a href="#">'+each.likes+'&nbsp&nbspLikes</a></div></div></div>';
 	});
 	$('#allPost').html(a.join(''));
 };
 
 var renderAllUsers = function(data){	
 	$('#name').html(data.name);
-	var a = data.map(function(each){
+	var users = data.map(function(each){
 		return '<option>'+each.username+'</option>'
 	});
-	$('#datalist1').html(a.join(''));
+	$('#datalist1').html(users.join(''));
 };
 
 var allPost = function(){
@@ -73,16 +94,21 @@ var follow = function(id){
 };
 
 var renderSearchResult = function(data){
-	return '<ul>'+data.map(function(each){
-		return '<li value='+each.id+' onclick=follow(this.getAttribute("value"))><pre>'+each.username+'        '+each.email+'</pre></li>'
-	}).join('')+'</ul>';
+	var template = '<a href="#" id="close">close</a><table class="userSrch">_TR_</table>';
+	var tableData = data.map(function(each){
+		return '<tr><td value='+each.id+' onclick=follow(this.getAttribute("value"))>'+each.username+'</td><td>'+each.email+'</td></tr>'
+	});
+	return template.replace('_TR_', tableData.join(''));
 };
 
 var search = function(){
 	var name = $('input[name=srch]').val();
 	$.post('userSearch', 'name='+name, function(data, status){
-		if(status == 'success')
-			$('#searchResult').html(renderSearchResult(data));
+		if(status == 'success'){
+			$('#top').removeClass('hide');
+			$('.message').html(renderSearchResult(data));
+			$('#close').on('click',close);
+		}
 	});
 };
 
@@ -92,15 +118,20 @@ var logout = function(){
 	});
 };
 
+var close = function(){
+	$('#top').addClass('hide');
+}
+
 $(document).ready(function(){
 	var interval = setInterval(allPost, 1000);
+	fillUsers();
 	$('#post').on('click', function(){
 		post($('textarea[name=post]').val());
 	});
-	fillUsers();
 	$('#search').on('click', search);
 	$('#logout').on('click', logout);
 	$.get('name', function(data, status){
-		$('.name').html(data);
-	})
+		$('.name').html('you are logged in as '+data);
+	});
+
 });	
