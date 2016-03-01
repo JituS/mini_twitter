1cb7cbbd22c5a306ba992b8be86bad0e8e956f9f
= <=>
2016-03-01 19:49:26 +0530

    opensift pg credential added

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
 var http = require('http');
 var app = require('./lib/routes.js');
-http.createServer(app).listen(3000);
+var server = http.createServer(app);
+server.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
